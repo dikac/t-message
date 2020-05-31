1adc17735bb4a5752a79c661c51267c3ad1ef44e
dikac <rigel234@yahoo.com>
2020-06-01 01:59:30 +0700

    allow git install & refresh compilation

--- dist/wrapper.d.ts
+++ dist/wrapper.d.ts
@@ -1,6 +1,6 @@
-import Message from "./message";
-export default class Wrapper<Msg, Container extends Message<Msg> = Message<Msg>> implements Readonly<Message<Msg>> {
+import Message, { Infer } from "./message";
+export default class Wrapper<Container extends Message = Message> implements Readonly<Message<Infer<Container>>> {
     messageContainer: Container;
     constructor(messageContainer: Container);
-    get message(): Msg;
+    get message(): Infer<Container>;
 }
